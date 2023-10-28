030a9bb6624e7cd3f088fccac01ceadfcb4ad204
Prabhat Kumar <prabhssgg3@gmail.com>
2023-10-28 22:55:48 +0530

    cssdone

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -1,9 +1,9 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
 export default function FancyCard() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.headingText}>Trending Places</Text>
       <View style={[styles.card, styles.cardElevated]}>
         <Image
@@ -13,9 +13,11 @@ export default function FancyCard() {
           }}
         />
         <View style={styles.cardBody}>
-          <Text style={styles.cardTitle}>mountains </Text>
-          <Text style={styles.cardLable}>himalaya,himachal </Text>
-          <Text style={styles.cardDescription}>Huge mountain of india and greatest mountain in the world   </Text>
+          <Text style={styles.cardTitle}>Mountains</Text>
+          <Text style={styles.cardLabel}>Himalaya, Himachal</Text>
+          <Text style={styles.cardDescription}>
+            Explore the majestic mountains of India, the tallest in the world.
+          </Text>
           <Text style={styles.cardFooter}>2 days away</Text>
         </View>
       </View>
@@ -24,13 +26,47 @@ export default function FancyCard() {
 }
 
 const styles = StyleSheet.create({
-  headingText: {},
-  card: {},
-  cardElevated: {},
-  cardImage: {height: 200, width: 300},
-  cardBody: {},
-  cardLable: {},
-  cardTitle: {},
-  cardDescription: {},
-  cardFooter: {},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 16,
+  },
+  card: {
+    width: 350,
+    borderRadius: 10,
+    marginVertical: 16,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    elevation: 4,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+  },
+  cardImage: {
+    height: 200,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  cardBody: {
+    padding: 16,
+  },
+  cardLabel: { color: 'dodgerblue', fontSize: 16 },
+  cardTitle: { color: 'purple', fontSize: 20, fontWeight: 'bold' },
+  cardDescription: {
+    color: 'black',
+    fontSize: 14,
+    marginTop: 8,
+  },
+  cardFooter: {
+    color: 'aqua',
+    fontSize: 14,
+    marginTop: 8,
+  },
 });
