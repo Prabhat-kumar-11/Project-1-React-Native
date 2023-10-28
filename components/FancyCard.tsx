90a436d73583e0b816dc9f470ca842497c57930b
Prabhat Kumar <prabhssgg3@gmail.com>
2023-10-28 22:22:04 +0530

    imagecardReady

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -6,9 +6,18 @@ export default function FancyCard() {
     <View>
       <Text style={styles.headingText}>Trending Places</Text>
       <View style={[styles.card, styles.cardElevated]}>
-        <Image style={styles.cardImage}
-        source={{uri:`https://images.pexels.com/photos/3574440/pexels-photo-3574440.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`}}
+        <Image
+          style={styles.cardImage}
+          source={{
+            uri: `https://images.pexels.com/photos/3574440/pexels-photo-3574440.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
+          }}
         />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>mountains </Text>
+          <Text style={styles.cardLable}>himalaya,himachal </Text>
+          <Text style={styles.cardDescription}>Huge mountain of india and greatest mountain in the world   </Text>
+          <Text style={styles.cardFooter}>2 days away</Text>
+        </View>
       </View>
     </View>
   );
@@ -18,5 +27,10 @@ const styles = StyleSheet.create({
   headingText: {},
   card: {},
   cardElevated: {},
-  cardImage:{height:200,width:300}
+  cardImage: {height: 200, width: 300},
+  cardBody: {},
+  cardLable: {},
+  cardTitle: {},
+  cardDescription: {},
+  cardFooter: {},
 });
