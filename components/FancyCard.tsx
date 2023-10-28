24a0ba8c22bb9f194e7c9114d02cb8f8b296b27d
Prabhat Kumar <prabhssgg3@gmail.com>
2023-10-28 22:11:10 +0530

    image

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -1,12 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
 export default function FancyCard() {
   return (
     <View>
-      <Text>FancyCard</Text>
+      <Text style={styles.headingText}>Trending Places</Text>
+      <View style={[styles.card, styles.cardElevated]}>
+        <Image style={styles.cardImage}
+        source={{uri:`https://images.pexels.com/photos/3574440/pexels-photo-3574440.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`}}
+        />
+      </View>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headingText: {},
+  card: {},
+  cardElevated: {},
+  cardImage:{height:200,width:300}
+});
